078503376ac707f2809a893059ed2fa268a23764
Tomek Cejner <tomek.cejner@gmail.com>
2014-07-14 00:24:00 +0200

    Initial Commit

---
+++ ActorWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActorWeather
+//
+//  Created by Tomek Cejner on 14/07/14.
+//  Copyright (c) 2014 Tomek Cejner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
